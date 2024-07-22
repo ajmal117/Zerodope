ffda4fa1d4a68f2a7491d482a657401d273ce739
ajmal ali <ajmal.mzn95@gmail.com>
2024-07-23 04:29:08 +0530

    login path

--- app/(auth)/_layout.js
+++ app/(auth)/_layout.js
@@ -13,9 +13,9 @@ export default function AppLayout() {
   // Only require authentication within the (app) group's layout as users
   // need to be able to access the (auth) group and sign in again.
 
-  // if (!session) {
-  //   return <Redirect href="/login" />;
-  // }
+  if (!session) {
+    return <Redirect href="/login" />;
+  }
 
   return (
     <Stack>
