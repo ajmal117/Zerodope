af0d33c1533b736fc0096abe27717ad0b04f0ed4
ajmal ali <ajmal.mzn95@gmail.com>
2024-08-15 18:16:44 +0530

    env addded

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,24 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel'],
+// module.exports = function(api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//     env: {
+//       production: {
+//         plugins: ['react-native-paper/babel'],
+//       },
+//     },
+//   };
+// };
+
+module.exports = {
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module:react-native-dotenv",
+      {
+        moduleName: "@env",
+        path: ".env",
       },
-    },
-  };
+    ],
+  ],
 };
